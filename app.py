f02b1cf73db2b72102551ba55bcd5b6e009afcba
Abraham Raji <avronr@tuta.io>
2021-08-04 04:53:13 +0530

    Contact form code refactoring
    In this commit:
    - New libs: ssl, email.mime.multipart, email.mime.text
    - Get the new Subject field
    - Emails now have proper headers
    - Emails will have the user email in Cc
    - Added comments
    - Better formatting for aesthetics

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
 from flask import Flask, render_template, request
-import os, smtplib
+import os, smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/success", methods=["POST"])
 def success():
+    # Get data from the form
     name = request.form.get("name")
     email = request.form.get("email")
+    subject = request.form.get("subject")
     phone = request.form.get("phone")
-    message = request.form.get("message")
+    text = request.form.get("message")
 
-    mail = f'Name: {name} \n Email: {email} \n Phone: {phone} \n {message}'
-    # Mailer
-    smtp_server = os.getenv('SMTP_SERVER')
-    smtp_port = os.getenv('SMTP_PORT')
-    email_addr = os.getenv('EMAIL_ADDR')
-    email_pass = os.getenv('EMAIL_PASS')
-    contact_email = os.getenv('TO_ADDR')
+    # Format the mail body
+    message = "Name: {} \nEmail: {} \nPhone: {} \n{}".format(name, email, phone, text)
+
+    # Configure mail headers
+    mail = MIMEMultipart()
+    mail["From"] = "admin@studevsoc.com"
+    mail["To"] = "contact@studevsoc.com"
+    mail["Subject"] = subject
+    mail["Cc"] = email
+    msg = MIMEText(message, "plain")
+    mail.attach(msg)
+
+    # information to connect to the mail server
+    smtp_server = "smtp.purelymail.com"
+    smtp_port = 587
+    email_addr = "admin@studevsoc.com"
+    email_pass = os.getenv("EMAIL_PASS")
+    contact_email = "contact@studevsoc.com"
+
+    # Connect to the Mail server
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.starttls()
     server.login(email_addr, email_pass)
-    server.sendmail(email_addr, contact_email, mail)
+    server.sendmail(email_addr, contact_email, mail.as_string())
+
+    # Let the user know that the transaction was successful
     return render_template("success.html", name=name)
+
 
 @app.route("/about")
 def about():
